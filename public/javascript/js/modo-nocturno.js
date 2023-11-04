de55d1b7b10d15acfa411dbcded9f733cf0befa1
gnzalobnites <50829446+gnzalobnites@users.noreply.github.com>
2023-11-04 09:53:34 -0300

    Update modo-nocturno.js

--- public/javascript/js/modo-nocturno.js
+++ public/javascript/js/modo-nocturno.js
@@ -28,6 +28,9 @@ function modoDiur(){
   botModoDiur.setAttribute("class", "d-none");
   let botModoNoct = document.getElementById("botModoNoct");
   botModoNoct.setAttribute("class", "d-block btn btn-outline-secondary");
+
+  let css_hightlight = document.getElementById("css_highlight");
+  css_highlight.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css";
   
   let navBar = document.getElementById("navSup");
   navBar.style.backgroundColor = "#fff";
