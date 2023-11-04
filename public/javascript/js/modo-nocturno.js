e413d90c13349aa42cd7d50805fe5704fd28e6c1
gnzalobnites <50829446+gnzalobnites@users.noreply.github.com>
2023-11-04 08:06:44 -0300

    Create modo-nocturno.js

---
+++ public/javascript/js/modo-nocturno.js
@@ -0,0 +1,42 @@
+function modoNoct(){
+  let botModoNoct = document.getElementById("botModoNoct");
+  botModoNoct.setAttribute("class", "d-none");
+  let botModoDiur = document.getElementById("botModoDiur");
+  botModoDiur.setAttribute("class", "d-block btn btn-outline-secondary");
+  
+  let navBar = document.getElementById("navBar");
+  navBar.style.backgroundColor = "#000";
+  navBar.setAttribute("class", "navbar navbar-dark bg-dark fixed-top");
+  navBar.style.color = "#fff";
+  
+  let offc = document.getElementById("offcanvasNavbar");
+  offc.style.backgroundColor = "#000";
+  offc.style.color = "#fff";
+  
+  let butClose = document.getElementById("butClose");
+  butClose.setAttribute("class", "btn-close btn-close-white");
+  
+  document.body.style.color = "#fff";
+  document.body.style.backgroundColor = "#000";
+}
+
+function modoDiur(){
+  let botModoDiur = document.getElementById("botModoDiur");
+  botModoDiur.setAttribute("class", "d-none");
+  let botModoNoct = document.getElementById("botModoNoct");
+  botModoNoct.setAttribute("class", "d-block btn btn-outline-secondary");
+  
+  let navBar = document.getElementById("navBar");
+  navBar.style.backgroundColor = "#fff";
+  navBar.setAttribute("class", "navbar bg-body-tertiary fixed-top");
+  
+  let offc = document.getElementById("offcanvasNavbar");
+  offc.style.backgroundColor = "#fff";
+  offc.style.color = "#000";
+  
+  let butClose = document.getElementById("butClose");
+  butClose.setAttribute("class", "btn-close");
+  
+  document.body.style.color = "#000";
+  document.body.style.backgroundColor = "#fff";
+    }
