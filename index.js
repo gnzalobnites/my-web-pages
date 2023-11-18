e7eb932ee41bbbdf176f1172c5744504d3ee6b94
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-11-17 21:34:40 -0300

    contenido javascript

--- index.js
+++ index.js
@@ -3,28 +3,41 @@ const path = require('path'); // Añade esta línea
 const PORT = process.env.PORT || 3030;
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer();
 var cosas = require('./cosas.js'); 
-var javascript_pug = require('./javascript.js'); 
+var js_pug_router = require('./public/javascript/javascript.js'); 
 const app = express();
 
 app.set('view engine', 'pug');
 
-app.set('views','./views'); 
+app.set('views','./views');
+
+app.set('views', [
+  './views',
+  './public/javascript' 
+]); 
 
 app.use(cookieParser())
 
-//Para analizar datos codificados en URL
-app.use(bodyParser.urlencoded({ extended: false })) 
+// para analizar application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 //Para analizar datos json
 app.use(bodyParser.json())
 
+// para analizar multipart/form-data
+app.use(upload.array());
+
 //Registrador simple de tiempo de solicitud 
 app.use(function(req, res, next){
-
   console.log("Una nueva solicitud recibida en " + Date.now());
-    
   next();
+});
+
+app.post('/form', function(req, res){
+  console.log(req.body);
+  res.send("recibí tu solicitud!");
 });
 
 // Configura la carpeta estática para tus archivos
@@ -40,6 +53,10 @@ app.get('/', function (req, res) {
 	      
 	// Enviar el archivo index.html como respuesta
     res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/form', function(req, res){
+  res.render('form');
 });
 
 app.get('/primera_plantilla', function(req, res){
@@ -68,7 +85,7 @@ app.get('/cosas/:id(\\d{5})', function(req, res){
   res.send('id: ' + req.params.id);
 });
 
-app.use('/javascript-pug', javascript_pug);
+app.use('/javascript-pug', js_pug_router);
 
 app.get('*', function(req, res){
   res.send('<h1>Lo siento, esta es una URL no válida.</h1>');
