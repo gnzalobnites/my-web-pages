b6798581209f1f5632e8e2390e4648947776ffe6
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-11-20 13:46:13 -0300

    probando cookies simples en la ruta /cookies

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const path = require('path'); // Añade esta línea
+const path = require('path'); 
 const PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var upload = multer();
 var cosas = require('./cosas.js'); 
 var js_pug_router = require('./public/javascript/javascript.js'); 
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/mi_db');
+var cookieParser = require('cookie-parser');
 var esquemaPersona = mongoose.Schema({
   nombre: String,
   edad: Number,
@@ -36,6 +35,11 @@ app.use(bodyParser.json())
 // para analizar multipart/form-data
 app.use(upload.array());
 
+app.get('/cookie', function(req, res){
+  //res.cookie('name', 'express').send(`<p><b>cookie configurada como <br> {name: ${req.cookies.name}}</b></p>`);
+  res.cookie('name', 'express').json(req.cookies)
+});
+
 app.get('/form', function(req, res){
   res.render('form');
 });
@@ -57,7 +61,7 @@ app.get('/', function (req, res) {
 	res.setHeader('Expires', '0');
 	      
 	// Enviar el archivo index.html como respuesta
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });                                                                                                                                            
 
 app.use('/javascript-pug', js_pug_router);
