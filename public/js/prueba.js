c32c80ab24cb9957d6c4fbba94a734121905ad93
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-11-25 17:46:09 -0300

    login reloj

--- public/js/prueba.js
+++ public/js/prueba.js
@@ -1,1 +1,8 @@
-alert("Hola desde /public/js")
+
+function mostrar_botones_sesión(){
+    //alert("Hola desde /public/js")
+    let div_int_reloj = document.getElementById('div_int_reloj');
+    div_int_reloj.setAttribute('class', 'gap-1 d-none');
+    let botones_sesion = document.getElementById('botones_sesion');
+    botones_sesion.setAttribute('class', 'gap-1 d-block');
+}
